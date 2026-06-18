f6c3d6d01f0ef099875aa4f46cf85a49938c77a9
Kabirraman <kabiraman72600@gmail.com>
2026-06-19 03:29:30 +0530

    feat: create user model and database migration

--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -2,19 +2,29 @@ package database
 
 import (
 	"os"
-
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"github.com/Kabirraman/DevBrain/internal/models"
+
 )
 
 var DB *gorm.DB
 
 func Connect() error {
+
 	var err error
 
 	DB, err = gorm.Open(
 		postgres.Open(os.Getenv("DATABASE_URL")),
 		&gorm.Config{},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	err = DB.AutoMigrate(
+		&models.User{},
 	)
 
 	return err
